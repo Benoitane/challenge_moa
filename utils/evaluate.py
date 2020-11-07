55bafb346215f0933c7b468501917983756db714
Benoitane <ane.benoit@gmail.com>
2020-11-07 18:18:31 +0100

    forget second option

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -7,13 +7,13 @@ def mean_log_loss(np_list_loss):
 
 def train_over_targets(features,targets,model,naive,list_pos_rate):
     models_loss = []
+    model.fit(features, targets[:,1])
+    naive.fit(features, targets[:,1])
     for i in range(targets.shape[1]):
         if list_pos_rate[i] >= 0.01:
-            model.fit(features, targets[:,i])
             Y_pred = model.predict(features)
             models_loss.append(log_loss(targets[:,i], Y_pred))
         if list_pos_rate[i] < 0.01:
-            naive.fit(features, targets[:,i])
             Y_pred = naive.predict(features)
-            models_loss.append(log_loss(features[:,i], Y_pred))
+            models_loss.append(log_loss(targets[:,i], Y_pred))
     return models_loss
