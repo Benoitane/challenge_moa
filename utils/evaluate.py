4026275a036cbc26d1f05445f688313a554d730c
Benoitane <ane.benoit@gmail.com>
2020-11-07 17:53:29 +0100

    train loop over cols for ML

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -4,3 +4,16 @@ from sklearn.metrics import log_loss
 
 def mean_log_loss(np_list_loss):
     return np.mean(np_list_loss)
+
+def train_over_targets(features,targets,model,naive,list_pos_rate):
+    models_loss = []
+    for i in range(targets.shape[i]):
+        if list_pos_rate[i] >= 0.01:
+            model.fit(features, targets[:,i])
+            Y_pred = clf.predict(features)
+            models_loss.append(log_loss(targets[:,i], Y_pred))
+        if list_pos_rate[i] < 0.01:
+            naive.fit(features, targets[:,i])
+            Y_pred = naive.predict(features)
+            models_loss.append(log_loss(features[:,i], Y_pred))
+    return models_loss
--- utils/models.py
+++ utils/models.py
@@ -1,9 +1,18 @@
-import pandas as pd
 import numpy as np
 
-def naive_classifier(features):
-    return np.zeros(features.shape[0])
+class NullPredictor:
+    def __init__(self, value):
+        self.value = value
 
+    def fit(self, dataset, labels):
+        try:
+            self.dimension = labels.shape[1]
+        except:
+            self.dimension = 1
 
+    def predict(self, X):
+        return self.value*np.ones((len(X),self.dimension))
 
-    
+    def score(self, X, y):
+        ypred = self.predict(X)
+        return np.sqrt(mean_squared_error(y,ypred))
